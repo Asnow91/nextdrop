1d7da2e2cae46355abf45320646eb1315541daa4
asnow <ale.raggetti@libero.it>
2025-09-16 17:40:06 +0200

    First commit: added project skeleton and .gitignore

---
+++ src/main/java/com/alera/nextdrop/NextDropApplication.java
@@ -0,0 +1,13 @@
+package com.alera.nextdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextDropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alera/nextdrop/NextDropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alera.nextdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextDropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
